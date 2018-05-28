bdba07e6ac2e09efbc611c365798d6e4faa93d37
hoonyeee <sim654@naver.com>
2018-05-28 11:10:09 +0900

    handler

--- app/src/main/java/com/hoonyeee/android/threadbasic/MainActivity.java
+++ app/src/main/java/com/hoonyeee/android/threadbasic/MainActivity.java
@@ -1,11 +1,14 @@
 package com.hoonyeee.android.threadbasic;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+
+import static com.hoonyeee.android.threadbasic.MainActivity.MSG_HIDE;
 
 public class MainActivity extends AppCompatActivity {
     ProgressBar progressBar;
@@ -23,10 +26,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
     //sub thread에서 ui thread접근하기위해
+    final static int MSG_HIDE = 0;
     Handler handler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
-            progressBar.setVisibility(View.GONE);
+            switch (msg.what) {
+                case MSG_HIDE:
+                    progressBar.setVisibility(View.GONE);
+                    Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                    startActivity(intent);
+                    break;
+            }
         }
     };
 }
@@ -37,13 +47,17 @@ class Count10Thread extends Thread{
         this.handler = handler;
     }
     public void run(){
-        for(int i=0; i<10; i++){
+        for(int i=0; i<3; i++){
             try {
                 Thread.sleep(1000);
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
         }
-        handler.sendEmptyMessage(0);
+        Message message = new Message();
+        message.what = MSG_HIDE;
+        message.arg1 = 35;
+        handler.sendMessage(message);
+        //handler.sendEmptyMessage(0);
     }
 }
