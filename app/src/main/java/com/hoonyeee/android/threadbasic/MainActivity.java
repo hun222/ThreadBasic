330fd043b89c97f4f497fdfe94a29296831a85ab
hoonyeee <sim654@naver.com>
2018-05-28 13:03:26 +0900

    AsyncTask

--- app/src/main/java/com/hoonyeee/android/threadbasic/MainActivity.java
+++ app/src/main/java/com/hoonyeee/android/threadbasic/MainActivity.java
@@ -1,63 +1,71 @@
 package com.hoonyeee.android.threadbasic;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import static com.hoonyeee.android.threadbasic.MainActivity.MSG_HIDE;
 
 public class MainActivity extends AppCompatActivity {
     ProgressBar progressBar;
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressBar = findViewById(R.id.progressBar);
+        new AsyncTask<Integer,Integer,Boolean>(){
+            @Override
+            protected void onPreExecute() {
+                //백그라운드 동작전에 세팅
+                super.onPreExecute();
+                progressBar = findViewById(R.id.progressBar);
+                tv = findViewById(R.id.tv);
+            }
 
-        Thread thread = new Count10Thread(handler);
-        thread.start();
+            @Override
+            protected Boolean doInBackground(Integer... args) {//Thread의 run
+                int time = 0;
+                if(args!=null && args.length >0)
+                    time = args[0];
+                for(int i=0; i<=10; i++){
+                    sleep(time);
+                    publishProgress(i); //onProgressUpdate호출
+                }
+                return true;
+            }
 
-
-    }
-    //sub thread에서 ui thread접근하기위해
-    final static int MSG_HIDE = 0;
-    Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case MSG_HIDE:
+            @Override
+            protected void onPostExecute(Boolean result) {
+                super.onPostExecute(result);
+                if(result){
                     progressBar.setVisibility(View.GONE);
-                    Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                    startActivity(intent);
-                    break;
+                }
             }
-        }
-    };
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                super.onProgressUpdate(values);
+                tv.setText(values[0]+"");
+            }
+
+            public void sleep(long time){
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }.execute(5);
+
+
+    }
+
 }
 
-class Count10Thread extends Thread{
-    Handler handler;
-    public Count10Thread(Handler handler){
-        this.handler = handler;
-    }
-    public void run(){
-        for(int i=0; i<3; i++){
-            try {
-                Thread.sleep(1000);
-            }catch (InterruptedException e){
-                e.printStackTrace();
-            }
-        }
-        Message message = new Message();
-        message.what = MSG_HIDE;
-        message.arg1 = 35;
-        handler.sendMessage(message);
-        //handler.sendEmptyMessage(0);
-    }
-}
+
